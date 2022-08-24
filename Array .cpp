32d6adc2c880a3b5435a214999bf8a6208fbef92
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-08-24 18:06:37 +0500

    Create Array .cpp

---
+++ Array .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[3];
+	a [0]=10;
+	cout<<"enter the value";
+	cin>>a[1];
+	cout<<a[0]<<endl;
+	cout<<a[1]<<endl;
+	cout<<"addition="<<a[0]+a[1]<<endl;
+	cout<<"subtraction="<<a[0]-a[1]<<endl;
+	cout<<"multiplication="<<a[0]*a[1]<<endl;
+	cout<<"divion="<<a[0]/a[1];
+}
