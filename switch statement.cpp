132d03d6a5d531cab8a41182d5f32aae626435d7
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-13 21:57:49 +0500

    Create switch statement.cpp

---
+++ switch statement.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int day;
+	cout<<"$$$$$$$$$$$$$$$$"<<endl;
+	cout<<"****************"<<endl;
+	cout<<"$$$$$$$$$$$$$$$$"<<endl;
+	cout<<"________________"<<endl3
+	;
+	cout<<"enter the integer";
+	cin>>day;
+	switch(day)
+	{
+		case 1:
+			cout<<"Monday";
+			break;
+				case2:
+					cout<<"Tuesday";
+					break;
+					case 3:
+						cout<<"Wednesday";
+						break;
+						case 4:
+							cout<<"Thursday";
+							break;
+							default:
+								cout<<"another";
+		
+	}
+
+
+}
+//coded by asma shahpal
