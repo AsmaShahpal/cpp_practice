2f49a46a2b484196a9858f4c23b964952f62101a
AsmaShahpal <111554716+AsmaShahpal@users.noreply.github.com>
2022-09-13 22:08:25 +0500

    Create simple code.cpp

---
+++ simple code.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int time =20;
+	if(time<18)
+	{
+		cout<<"Good Day";
+		
+	}
+	else
+	{
+		cout<<"Goodevening";
+	}
+
+
+}
+//coded by asma shahpal
